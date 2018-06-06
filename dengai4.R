7ba81c0359ca94236a588da1372b5cc16835d28b
Stewart Adams <adamssl@gmail.com>
2018-06-07 07:37:09 +1000

    Total lag coverage

---
+++ dengai4.R
@@ -0,0 +1,383 @@
+rm(list=ls())
+pkgs <- c('tidyverse','tidyimpute', 'tibbletime', 'corrplot', 'magrittr', 'zoo', 'RColorBrewer', 'gridExtra','MASS','mice', 'forecast')
+invisible(lapply(pkgs, require, character.only = T))
+rm(pkgs)
+select <- dplyr::select
+
+setwd("/temp/NU/413/dengai")
+#X <- read.csv("dengue_features_train.csv") #reads in as a list...
+train.X <- read_csv("dengue_features_train.csv")
+train.y <- read_csv("dengue_labels_train.csv")
+
+test.X <- read_csv('dengue_features_test.csv')
+
+#train.X %<>% left_join(train.y, by=c('city', 'year', 'weekofyear')) #brings the response into same frame
+
+#=====================================================================================
+#             Cases on 53-week calendar, rest of data is on 52. Let's combine
+#=====================================================================================
+train.X %<>% filter(weekofyear!=53) #It was all blank data previously, safe to just drop
+
+#create a holder variable to mark where we need to forecast deaths in the test set...
+test.X.week53 <- test.X %>% filter(weekofyear==53)
+test.X %<>% filter(weekofyear!=53)
+
+joinyear <- train.y %>% filter(weekofyear==53) %>% select(year) %>% as.list()
+joinyear <- joinyear$year
+incity <- train.y %>% filter(weekofyear==53) %>% select(city) %>% as.list()
+incity <- incity$city
+
+mergecases <- train.y %>% filter(year %in% joinyear & weekofyear==1) %>% select(total_cases) +
+  train.y %>% filter(weekofyear==53) %>% select(total_cases) %>% as.list()
+mergecases <- mergecases$total_cases
+
+for (i in (1:length(joinyear))) {
+  #curr_deaths <- train.y[(train.y$weekofyear==1 & train.y$year==(joinyear$year[i] + 1) & train.y$city==incity[i]),'total_cases']
+  train.y[(train.y$weekofyear==1 & train.y$year==joinyear[i] & train.y$city==incity[i]),'total_cases'] <-
+    as.integer(mergecases[i])
+}
+#now get rid of the week-53 in the training set...
+train.y %<>% filter(weekofyear!=53)
+
+rm(i, incity, joinyear, mergecases)
+
+
+#=====================================================================================
+#             Missing data? Perform imputation
+#=====================================================================================
+full.X <- rbind(train.X, test.X)
+
+is_missing <- function(input.data){
+  input.data %>% group_by(city) %>%
+    summarise_all(funs(round(100*mean(is.na(.)),2))) %>% t %>% as.data.frame()
+}
+
+# count missing values (as percent)
+full.X %>% is_missing()
+full.X.imp <- mice(data=full.X, method = 'rf', ntree=3, m=1)
+full.X.comp <- as.tibble(complete(full.X.imp, 1))
+full.X.comp %>% is_missing()
+#reanalysis_sat_precip_amt_mm could not be fully imputed, let's not use that predictor
+full.X.comp[is.na(full.X.comp$reanalysis_sat_precip_amt_mm),] %>% glimpse()
+rm(full.X.imp)
+
+full.X.comp %<>% select(-reanalysis_sat_precip_amt_mm)
+
+
+#=====================================================================================
+#             Create features
+#=====================================================================================
+add_group_summaries <- function(d, 
+                                groupingVars, 
+                                ...) {
+  groupingSyms <- rlang::syms(groupingVars)
+  d <- ungroup(d) # just in case
+  dg <- group_by(d, !!!groupingSyms)
+  ds <- summarize(dg, ...)
+  ds <- ungroup(ds)
+  left_join(d, ds, by= groupingVars)
+}
+
+#seasonal variables
+full.X.comp %<>% mutate(season= case_when(
+  weekofyear > (52-4) ~ as.integer(0),
+  weekofyear > (52-4-12) ~ as.integer(3),
+  weekofyear > (52-4-12-12) ~ as.integer(2),
+  weekofyear > (52-4-12-12-12) ~ as.integer(1),
+  TRUE ~ as.integer(0)
+))
+
+full.X.comp %<>% add_group_summaries(c('city', 'year', 'season'),
+                                     seas_min_temp = min(reanalysis_min_air_temp_k),
+                                     seas_max_temp = mean(reanalysis_max_air_temp_k),
+                                     seas_avg_temp = max(reanalysis_avg_temp_k))
+
+
+full.sj <- full.X.comp %>% filter(city=='sj')
+full.iq <- full.X.comp %>% filter(city=='iq')
+
+library(data.table)
+
+
+zz<-full.sj %>% select(-c(city, year,weekofyear, week_start_date, season))
+zz.colnames <- zz%>%colnames()
+setDT(zz)[,paste0('ndvi_ne', 1:4) := shift(ndvi_ne, 1:4)][]
+setDT(zz)[,paste0('ndvi_nw', 1:4) := shift(ndvi_nw, 1:4)][]
+setDT(zz)[,paste0('ndvi_se', 1:4) := shift(ndvi_se, 1:4)][]
+setDT(zz)[,paste0('ndvi_sw', 1:4) := shift(ndvi_sw, 1:4)][]
+setDT(zz)[,paste0('precipitation_amt_mm', 1:4) := shift(precipitation_amt_mm, 1:4)][]
+setDT(zz)[,paste0('reanalysis_air_temp_k', 1:4) := shift(reanalysis_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_avg_temp_k', 1:4) := shift(reanalysis_avg_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_dew_point_temp_k', 1:4) := shift(reanalysis_dew_point_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_max_air_temp_k', 1:4) := shift(reanalysis_max_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_min_air_temp_k', 1:4) := shift(reanalysis_min_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_precip_amt_kg_per_m2', 1:4) := shift(reanalysis_precip_amt_kg_per_m2, 1:4)][]
+setDT(zz)[,paste0('reanalysis_relative_humidity_percent', 1:4) := shift(reanalysis_relative_humidity_percent, 1:4)][]
+setDT(zz)[,paste0('reanalysis_tdtr_k', 1:4) := shift(reanalysis_tdtr_k, 1:4)][]
+setDT(zz)[,paste0('station_avg_temp_c', 1:4) := shift(station_avg_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_max_temp_c', 1:4) := shift(station_max_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_min_temp_c', 1:4) := shift(station_min_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_precip_mm', 1:4) := shift(station_precip_mm, 1:4)][]
+setDT(zz)[,paste0('seas_min_temp', 1:4) := shift(seas_min_temp, 1:4)][]
+setDT(zz)[,paste0('seas_max_temp', 1:4) := shift(seas_max_temp, 1:4)][]
+setDT(zz)[,paste0('seas_avg_temp', 1:4) := shift(seas_avg_temp, 1:4)][]
+full.sj <- inner_join(full.sj, zz)
+
+zz<-full.iq %>% select(-c(city, year,weekofyear, week_start_date, season))
+zz.colnames <- zz%>%colnames()
+setDT(zz)[,paste0('ndvi_ne', 1:4) := shift(ndvi_ne, 1:4)][]
+setDT(zz)[,paste0('ndvi_nw', 1:4) := shift(ndvi_nw, 1:4)][]
+setDT(zz)[,paste0('ndvi_se', 1:4) := shift(ndvi_se, 1:4)][]
+setDT(zz)[,paste0('ndvi_sw', 1:4) := shift(ndvi_sw, 1:4)][]
+setDT(zz)[,paste0('precipitation_amt_mm', 1:4) := shift(precipitation_amt_mm, 1:4)][]
+setDT(zz)[,paste0('reanalysis_air_temp_k', 1:4) := shift(reanalysis_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_avg_temp_k', 1:4) := shift(reanalysis_avg_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_dew_point_temp_k', 1:4) := shift(reanalysis_dew_point_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_max_air_temp_k', 1:4) := shift(reanalysis_max_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_min_air_temp_k', 1:4) := shift(reanalysis_min_air_temp_k, 1:4)][]
+setDT(zz)[,paste0('reanalysis_precip_amt_kg_per_m2', 1:4) := shift(reanalysis_precip_amt_kg_per_m2, 1:4)][]
+setDT(zz)[,paste0('reanalysis_relative_humidity_percent', 1:4) := shift(reanalysis_relative_humidity_percent, 1:4)][]
+setDT(zz)[,paste0('reanalysis_tdtr_k', 1:4) := shift(reanalysis_tdtr_k, 1:4)][]
+setDT(zz)[,paste0('station_avg_temp_c', 1:4) := shift(station_avg_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_max_temp_c', 1:4) := shift(station_max_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_min_temp_c', 1:4) := shift(station_min_temp_c, 1:4)][]
+setDT(zz)[,paste0('station_precip_mm', 1:4) := shift(station_precip_mm, 1:4)][]
+setDT(zz)[,paste0('seas_min_temp', 1:4) := shift(seas_min_temp, 1:4)][]
+setDT(zz)[,paste0('seas_max_temp', 1:4) := shift(seas_max_temp, 1:4)][]
+setDT(zz)[,paste0('seas_avg_temp', 1:4) := shift(seas_avg_temp, 1:4)][]
+full.iq <- inner_join(full.iq, zz)
+
+
+
+
+nrow.sj.train <- train.X %>% filter(city=='sj') %>% nrow()
+nrow.sj.test <- test.X %>% filter(city=='sj') %>% nrow()
+nrow.iq.train <- train.X %>% filter(city=='iq') %>% nrow()
+nrow.iq.test <- test.X %>% filter(city=='iq') %>% nrow()
+sj.train <- head(full.sj, n=nrow.sj.train)
+sj.test <- tail(full.sj, n=nrow.sj.test)
+sj.cases <- train.y %>% filter(city=='sj') %>% select(total_cases)
+iq.train <- head(full.iq, n=nrow.iq.train)
+iq.test <- tail(full.iq, n=nrow.iq.test)
+iq.cases <- train.y %>% filter(city=='iq') %>% select(total_cases)
+
+
+xreg.full <- full.sj %>% select(-c(city,year,week_start_date)) %>% colnames()
+
+
+sj.train <- head(full.sj, n=nrow.sj.train)
+sj.test <- tail(full.sj, n=nrow.sj.test)
+iq.train <- head(full.iq, n=nrow.iq.train)
+iq.test <- tail(full.iq, n=nrow.iq.test)
+
+full.train <- rbind(sj.train, iq.train)
+full.test <- rbind(sj.test, iq.test)
+full.X <- rbind(full.train, full.test)
+
+
+#Now break them back into original structure train / test set, now that feature creation has finished
+train.X <- head(full.X, n=nrow(train.X))
+test.X <- tail(full.X, n=nrow(test.X))
+
+#=====================================================================================
+#             Split data set into two cities, setup case lags
+#=====================================================================================
+train.X.sj <- train.X %>% filter(city=='sj')
+train.y.sj <- train.y %>% filter(city=='sj')
+train.X.iq <- train.X %>% filter(city=='iq')
+train.y.iq <- train.y %>% filter(city=='iq')
+
+test.X.sj <- test.X %>% filter(city=='sj')
+test.X.iq <- test.X %>% filter(city=='iq')
+
+
+
+train.X.sj %<>% mutate('total_cases' = train.y.sj$total_cases)
+train.X.sj$case_lag1 <- lag(train.X.sj$total_cases, 1)
+train.X.sj$case_lag2 <- lag(train.X.sj$total_cases, 2)
+train.X.sj$case_lag3 <- lag(train.X.sj$total_cases, 3)
+train.X.iq %<>% mutate('total_cases' = train.y.iq$total_cases)
+train.X.iq$case_lag1 <- lag(train.X.iq$total_cases, 1)
+train.X.iq$case_lag2 <- lag(train.X.iq$total_cases, 2)
+train.X.iq$case_lag3 <- lag(train.X.iq$total_cases, 3)
+
+
+test.X.sj$predicted <- 0
+test.X.sj$case_lag1 <- 0
+test.X.sj$case_lag2 <- 0
+test.X.sj$case_lag3 <- 0
+test.X.sj$case_lag1[1] <- train.X.sj$total_cases[nrow(train.X.sj)]
+test.X.sj$case_lag2[1] <- train.X.sj$total_cases[(nrow(train.X.sj)-1)]
+test.X.sj$case_lag2[2] <- train.X.sj$total_cases[(nrow(train.X.sj))]
+test.X.sj$case_lag3[1] <- train.X.sj$total_cases[(nrow(train.X.sj)-2)]
+test.X.sj$case_lag3[2] <- train.X.sj$total_cases[(nrow(train.X.sj)-1)]
+test.X.sj$case_lag3[3] <- train.X.sj$total_cases[(nrow(train.X.sj))]
+
+test.X.iq$predicted <- 0
+test.X.iq$case_lag1 <- 0
+test.X.iq$case_lag2 <- 0
+test.X.iq$case_lag3 <- 0
+test.X.iq$case_lag1[1] <- train.X.iq$total_cases[nrow(train.X.iq)]
+test.X.iq$case_lag2[1] <- train.X.iq$total_cases[(nrow(train.X.iq)-1)]
+test.X.iq$case_lag2[2] <- train.X.iq$total_cases[(nrow(train.X.iq))]
+test.X.iq$case_lag3[1] <- train.X.iq$total_cases[(nrow(train.X.iq)-2)]
+test.X.iq$case_lag3[2] <- train.X.iq$total_cases[(nrow(train.X.iq)-1)]
+test.X.iq$case_lag3[3] <- train.X.iq$total_cases[(nrow(train.X.iq))]
+
+
+#=====================================================================================
+#             Prepare for modelling
+#=====================================================================================
+
+
+get_best_model <- function(input.data, input.xreg, grid.len=3) {
+  best_mae <- 1000
+  true.mae = vector()
+  split <- floor(nrow(input.data)*.2)
+  X.train <- ts(input.data$total_cases[1:(nrow(input.data)-split)], frequency = 52)
+  X.test <- ts(input.data$total_cases[(nrow(input.data)-split):nrow(input.data)], frequency = 52, start=(end(X.train) + c(0,1)))
+  xreg.data = input.data %>% select(input.xreg)
+  xreg.train <- xreg.data[1:(nrow(input.data)-split),]
+  xreg.test <- xreg.data[(nrow(input.data)-split):nrow(input.data),]
+  grid = seq(1:grid.len)
+  
+  
+  for (i in grid) {
+    X.fit <- nnetar(X.train, p=1, repeats = 2, scale.inputs = TRUE, xreg=xreg.train, MaxNWts=6000)
+    X.fc <- forecast(X.fit, h=split, xreg=xreg.test)
+    model.mae <- accuracy(X.fc)[3]
+    
+    #for (j in seq((nrow(input.data)-split),(nrow(input.data)-1) )) {
+    for (j in 1:nrow(xreg.test) ) {
+      #print(j)
+      point.fc <- forecast(X.fit, h=1, xreg=xreg.test[j,])
+      point.fc <- as.numeric(point.fc$mean)
+      if(point.fc < 0) {point.fc <- 0}
+      point.mae <- abs(point.fc - X.test[j])
+      true.mae <- append(true.mae, point.mae)
+      #paste(j, ":", true.mae)
+      # test.X.iq$predicted[i] <- point.fc
+      # xreg.iq$case_lag1[i+1] <- point.fc
+    }
+    
+    #print(mean(true.mae))
+    
+    mae <- mean(true.mae)
+    if (mae < best_mae) {
+      best_fit <- X.fit
+      best_mae <- mae
+    }
+    cat('Iteration:', i, 'model.mae:', model.mae, ' true mae:', mae, '\n')
+  }
+  X.fit <- nnetar(X.train, xreg=xreg.train, maxit=256, model = best_fit, MaxNWts=6000)
+  X.fc <- forecast(X.fit, h=split, xreg=xreg.test)
+  plot(X.fc, type='l')
+  lines(X.test,col='red')
+  X.fit <- nnetar(ts(input.data$total_cases, frequency=52), xreg=xreg.data, model = best_fit, MaxNWts=6000)
+  return(X.fit)
+}
+
+validate_perf <- function(input.model, input.xreg, input.train.X) {
+  #best_model <- get_best_model(train.X.sj, cust.xreg)
+  #xreg = train.X.sj %>% select(cust.xreg)
+  model.predicted <- input.train.X$total_cases
+  
+  split <- floor(nrow(input.xreg)*.8)
+  
+  for (i in (split:nrow(input.xreg))) {
+    point.fc <- forecast(input.model, h=1, xreg=input.xreg[i,])
+    point.fc <- as.numeric(point.fc$mean)
+    if(point.fc < 0) {point.fc <- 0}
+    model.predicted[i] <- point.fc
+    if (i<(nrow(input.xreg)-1)) {
+      input.xreg$case_lag1[i+1] <- point.fc
+      if (i<(nrow(input.xreg)-2) & 'case_lag2' %in% colnames(input.xreg)) {
+        input.xreg$case_lag2[i+2] <- point.fc
+        if (i<(nrow(input.xreg)-3) & 'case_lag3' %in% colnames(input.xreg)) {
+          input.xreg$case_lag3[i+3] <- point.fc
+        }
+      }
+    }
+  }
+  print(mean(abs(model.predicted - input.train.X$total_cases)))
+  plot(input.train.X$total_cases, type='l', xlim=c(split,nrow(input.xreg)))
+  lines(model.predicted, col='blue')
+}
+
+
+full_model <- get_best_model(train.X.sj, c(xreg.full,'case_lag1'),1)
+validate_perf(full_model, train.X.sj %>% select(c(xreg.full,'case_lag1')), train.X.sj)
+
+xreg.sj = test.X.sj %>% select(c(xreg.full,'case_lag1'))
+for (i in (1:(nrow(test.X.sj)))) {
+  point.fc <- forecast(full_model, h=1, xreg=xreg.sj[i,])
+  point.fc <- as.numeric(point.fc$mean)
+  if(point.fc < 0) {point.fc <- 0}
+  test.X.sj$predicted[i] <- point.fc
+  if (i<(nrow(test.X.sj)-1)) {
+    xreg.sj$case_lag1[i+1] <- point.fc
+    if (i<(nrow(test.X.sj)-2) & 'case_lag2' %in% colnames(xreg.sj)) {
+      xreg.sj$case_lag2[i+2] <- point.fc
+      if (i<(nrow(test.X.sj)-3) & 'case_lag3' %in% colnames(xreg.sj)) {
+        xreg.sj$case_lag3[i+3] <- point.fc
+      }
+    }
+  }
+}
+plot(test.X.sj$predicted, type='l', main='SJ predicted cases')
+
+
+full_model <- get_best_model(train.X.sj, c(xreg.full,'case_lag1'),1)
+validate_perf(full_model, train.X.sj %>% select(c(xreg.full,'case_lag1')), train.X.sj)
+
+xreg.sj = test.X.sj %>% select(c(xreg.full,'case_lag1'))
+for (i in (1:(nrow(test.X.sj)))) {
+  point.fc <- forecast(full_model, h=1, xreg=xreg.sj[i,])
+  point.fc <- as.numeric(point.fc$mean)
+  if(point.fc < 0) {point.fc <- 0}
+  test.X.sj$predicted[i] <- point.fc
+  if (i<(nrow(test.X.sj)-1)) {
+    xreg.sj$case_lag1[i+1] <- point.fc
+    if (i<(nrow(test.X.sj)-2) & 'case_lag2' %in% colnames(xreg.sj)) {
+      xreg.sj$case_lag2[i+2] <- point.fc
+      if (i<(nrow(test.X.sj)-3) & 'case_lag3' %in% colnames(xreg.sj)) {
+        xreg.sj$case_lag3[i+3] <- point.fc
+      }
+    }
+  }
+}
+plot(test.X.sj$predicted, type='l', main='SJ predicted cases')
+
+
+full_model <- get_best_model(train.X.iq, c(xreg.full,'case_lag1'),1)
+validate_perf(full_model, train.X.iq %>% select(c(xreg.full,'case_lag1')), train.X.iq)
+
+xreg.iq = test.X.iq %>% select(c(xreg.full,'case_lag1'))
+for (i in (1:(nrow(test.X.iq)))) {
+  point.fc <- forecast(full_model, h=1, xreg=xreg.iq[i,])
+  point.fc <- as.numeric(point.fc$mean)
+  if(point.fc < 0) {point.fc <- 0}
+  test.X.iq$predicted[i] <- point.fc
+  if (i<(nrow(test.X.iq)-1)) {
+    xreg.iq$case_lag1[i+1] <- point.fc
+    if (i<(nrow(test.X.iq)-2) & 'case_lag2' %in% colnames(xreg.iq)) {
+      xreg.iq$case_lag2[i+2] <- point.fc
+      if (i<(nrow(test.X.iq)-3) & 'case_lag3' %in% colnames(xreg.iq)) {
+        xreg.iq$case_lag3[i+3] <- point.fc
+      }
+    }
+  }
+}
+plot(test.X.iq$predicted, type='l', main='iq predicted cases')
+
+submission = read.csv('submission_format.csv')
+inner_join(submission, rbind(test.X.sj,test.X.iq), by=c('city', 'year', 'weekofyear')) %>%
+  dplyr::select(city, year, weekofyear, total_cases=predicted) ->
+  prediction.df
+
+prediction.df$total_cases <- round(prediction.df$total_cases)
+
+write.csv(prediction.df, file='AdamsStewartSubmission.csv', row.names = FALSE)
+
+
+
